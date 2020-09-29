5ea1f044fd46bdde042541db2191cf92a784f814
Wataru-Ki <wataru_ru@outlook.jp>
2020-09-29 12:10:00 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #未ログインユーザーをログイン画面に促す
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #新規登録時（sign_up）に名前を入れる要素を追加
+  end
 end
